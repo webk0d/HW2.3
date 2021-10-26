6e99ab58884869a893a8725b989c8880fd2d179f
SergeyChumakov <webk0d@ya.ru>
2021-10-26 18:24:28 +0300

    Initial Commit

---
+++ HW2.3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW2.3
+//
+//  Created by Сергей Чумаков on 26.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
